e0da3a5d8d9aaa484c1d5f425546953d136436fc
yu jieun <wksrma1@naver.com>
2016-11-17 00:43:50 +0900

    loger02

---
+++ loger02/src/main/java/com/example/myloger/InputActivity.java
@@ -0,0 +1,8 @@
+package com.example.myloger;
+
+/**
+ * Created by 유지은 on 2016-11-16.
+ */
+
+public class InputActivity {
+}
--- loger02/src/main/java/com/example/myloger/MapsActivity.java
+++ loger02/src/main/java/com/example/myloger/MapsActivity.java
@@ -1,46 +1,134 @@
 package com.example.myloger;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.LinkedList;
+
+import static com.example.myloger.R.id.map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LinkedList<LocationNode> location_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used
+        location_list = new LinkedList<LocationNode>();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(map);
         mapFragment.getMapAsync(this);
+
+        startLocationService();
+
+
+
     }
 
+    private void startLocationService() {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 10000;
+        //최소시간 설정 10분으로 하려면 600000으로 설정한다
+        float minDistance = 0;
+
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        manager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                minTime,
+                minDistance,
+                gpsListener);
+
+    }
+    private class GPSListener implements LocationListener {
+        /**
+         * 위치 정보가 확인되었을 때 호출되는 메소드
+         */
+
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            showCurrentLocation(latitude, longitude);
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+        private void showCurrentLocation(Double latitude, Double longitude) {
+            LatLng curPoint = new LatLng(latitude, longitude);
+
+            final LocationNode tmp_location = new LocationNode(latitude, longitude);
+            location_list.add(tmp_location);
+
+
+
+            Button btn = (Button) (findViewById(R.id.button2));
+            btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    EditText edit =(EditText) findViewById(R.id.editText);
+                    if (v.isClickable()) {
+                        Marker marker = mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(tmp_location.GetLatitude(),tmp_location.GetLongtitude()))
+                                .title(edit.getText().toString())
+
+                        );
+                    }
+                }
+            });
+
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 17));
+
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
